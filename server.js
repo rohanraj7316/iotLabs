344c4732ecd596254b7893c38566d1f7b54e281f
rohanraj7316 <rohanraj7316@gmail.com>
2017-07-03 18:14:21 +0530

    implementing socket.io server listener

--- server.js
+++ server.js
@@ -3,3 +3,35 @@
  */
 
 
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8080,function(){
+  console.log('server is listening to port number 7000');
+});
+
+function handler(req,res) {
+  console.log('inside handler');
+}
+
+io.on('connection',function (socket) {
+  console.log('connection is stablished');
+
+  socket.on('disconnect',function(){
+    console.log('connection got disconnect');
+  });
+
+  socket.on('data',function(data){
+    console.log('inside data listener: ', data);
+  });
+
+  socket.on('close',function(){
+    console.log('socket connection closed');
+  });
+
+  socket.on('error',function(err){
+    console.log('error occure in socket connection');
+  });
+
+})
