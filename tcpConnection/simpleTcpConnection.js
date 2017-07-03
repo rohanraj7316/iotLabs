1ca7e6991acf2bb337bac08fe3155a6bbdb22730
rohanraj7316 <rohanraj7316@gmail.com>
2017-07-03 16:00:13 +0530

    there are some changes in tcpConnection Folder in simpleTcpConnection file

--- tcpConnection/simpleTcpConnection.js
+++ tcpConnection/simpleTcpConnection.js
@@ -1,27 +1,27 @@
 /**
  * Created by unio-raj on 3/7/17.
  */
-var portnumber,ipaddress;
+//var portnumber,ipaddress;
 var net = require('net');
 var server = net.createServer(function (socket) {
    socket.write('Echo Server');
    socket.pipe(socket);
 });
 
-server.listen(portnumber,ipaddress);
+server.listen(8080,'52.43.200.112');
 
-var net = require('net');
-
-var client = new net.Socket();
-client.connect(portnumber,ipaddress,function () {
-   console.log('Connected');
-});
-
-client.on('data',function (data) {
-   console.log('Recieved Data' + data);
-   client.destroy();
-});
-
-client.on('close',function () {
-   console.log('connection closed')
-});
+// var net = require('net');
+//
+// var client = new net.Socket();
+// client.connect(8080,'52.43.200.112',function () {
+//    console.log('Connected');
+// });
+//
+// client.on('data',function (data) {
+//    console.log('Recieved Data' + data);
+//    client.destroy();
+// });
+//
+// client.on('close',function () {
+//    console.log('connection closed')
+// });
